3e6466e8230d6e3ac5c1ffd44e0769aa09f40d13
bhavin_avdevs <bhavin@avdevs.com>
2016-05-24 12:48:26 +0530

    spacing manage

--- attributes/radio_button/form.php
+++ attributes/radio_button/form.php
@@ -4,12 +4,12 @@
  * RadioButton .
  */
 
-	$form = Core::make('helper/form');;
-	$options = array();
-	foreach($controller->getOptions() as $option) {
-		echo '<div class="clearfix">';
-		$options[$option->getRadioButtonAttributeOptionID()] = $option->getRadioButtonAttributeOptionDisplayValue();
-        echo $form->radio($view->field('atRadioButtonOptionValue'), $option->getRadioButtonAttributeOptionID(), $selectedRadioButtonOptions[0]);
-        echo '&nbsp;'.$form->label('', $option->getRadioButtonAttributeOptionDisplayValue());
-		echo '</div>';
-	}
+$form = Core::make('helper/form');;
+$options = array();
+foreach($controller->getOptions() as $option) {
+    echo '<div class="clearfix">';
+    $options[$option->getRadioButtonAttributeOptionID()] = $option->getRadioButtonAttributeOptionDisplayValue();
+    echo $form->radio($view->field('atRadioButtonOptionValue'), $option->getRadioButtonAttributeOptionID(), $selectedRadioButtonOptions[0]);
+    echo '&nbsp;'.$form->label('', $option->getRadioButtonAttributeOptionDisplayValue()).'&nbsp;&nbsp;&nbsp;&nbsp;&nbsp;';
+    echo '</div>';
+}
